d61fccb778de6333c3b1ebd40e869872d2085c37
Titus Chu <tituschu@uchicago.edu>
2023-05-05 07:16:34 +0000

    sign up works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+
+  def new_registration_form
+
+
+    render({ :template => "users/signup_form.html" })
+  end
+
   def index
     @users = User.all.order({ :username => :asc })
 
@@ -16,10 +23,16 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
 
-    user.save
+    save_status = user.save
 
-    redirect_to("/users/#{user.username}")
+    if save_status == true
+      redirect_to("/users/#{user.username}", {:notice => "Welcome, " + user.username + "!"})
+    else
+      redirect_to("/user_sign_up", {:alert => user.errors.full_messages.to_sentence})
+    end
   end
 
   def update
